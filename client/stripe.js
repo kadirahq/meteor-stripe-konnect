a3240225aa04b73ab418f27b113998dd3bc64a57
thinkholic <mailtokasun@gmail.com>
2015-06-19 17:10:18 +0530

    separated Stripe call to new function

--- client/stripe.js
+++ client/stripe.js
@@ -1,24 +1,29 @@
-loadScript('https://js.stripe.com/v2/', function (err, script) {
+loadScript('https://js.stripe.com/v2/', function (err) {
   if (!err) {
-    loadScript('https://checkout.stripe.com/checkout.js', function (err, script) {
+    loadScript('https://checkout.stripe.com/checkout.js', function (err) {
       if (!err) {
-        Meteor.startup(function () {
-
-          Stripe = window.Stripe;
-          StripeCheckout = window.StripeCheckout;
-          var config = __meteor_runtime_config__.stripe;
-
-          Meteor.defer(function() {
-            Stripe.getCheckoutHandler = function (options) {
-              return StripeCheckout.configure(_.defaults(_.clone(options), {
-                name: config.appName,
-                key: config.publishableKey,
-                image: config.appLogo,
-              }));
-            };
-          });
-        });
+        loadStripeAPI();
+      } else {
+        throw new Meteor.Error("Stripe resources loading failed."); 
       }
     });
+  } else {
+    throw new Meteor.Error("Stripe resources loading failed.");
   }
 });
+
+function loadStripeAPI() {
+  Meteor.startup(function () {
+    Stripe = window.Stripe;
+    StripeCheckout = window.StripeCheckout;
+    var config = __meteor_runtime_config__.stripe;
+
+    Stripe.getCheckoutHandler = function (options) {
+      return StripeCheckout.configure(_.defaults(_.clone(options), {
+        name: config.appName,
+        key: config.publishableKey,
+        image: config.appLogo,
+      }));
+    };
+  });
+}
