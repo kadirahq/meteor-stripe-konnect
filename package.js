ae38df62e0fdaf8fe1a15c155faf6d42984a19e8
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-04-17 21:38:55 +0530

    v3.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Stripe for Meteor Apps",
-  version: "3.0.0",
+  version: "3.1.0",
   name: "kadira:stripe-konnect",
   git: 'https://github.com/kadirahq/meteor-stripe-konnect.git'
 });
